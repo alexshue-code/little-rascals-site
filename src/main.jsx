b4a4fd2ce0cfda3fe2f5117b5ddcf7582343dd96
alexshue-code <thealexshue@gmail.com>
2026-04-13 17:51:17 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import PetHospitalHomepage from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <PetHospitalHomepage />
 )
